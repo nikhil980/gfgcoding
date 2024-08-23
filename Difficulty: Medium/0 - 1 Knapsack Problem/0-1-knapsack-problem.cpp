212a4da4692549929932e948b09333275c6b7d1c
nikhil980 <154825911+nikhil980@users.noreply.github.com>
2024-08-24 04:40:43 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
@@ -0,0 +1,74 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+  class Solution {
+  public:
+    // Function to return max value that can be put in knapsack of capacity W.
+    int knapSack(int W, vector<int>& wt, vector<int>& val) 
+    {
+        int n = val.size();
+        vector<vector<int>> dp(n + 1, vector<int>(W + 1, 0));
+
+        for (int i = 0; i <= n; i++) {
+            for (int w = 0; w <= W; w++) {
+                if (i == 0 || w == 0)
+                    dp[i][w] = 0;
+                else if (wt[i - 1] <= w)
+                    dp[i][w] = max(val[i - 1] + dp[i - 1][w - wt[i - 1]], dp[i - 1][w]);
+                else
+                    dp[i][w] = dp[i - 1][w];
+            }
+        }
+
+        return dp[n][W];
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    // taking total testcases
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        // reading number of elements and weight
+        int n, w;
+        vector<int> arr, val, wt, drr;
+        string ip;
+        int number;
+        getline(cin, ip);
+        stringstream ss(ip);
+
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        getline(cin, ip);
+        ss.clear();
+        ss.str(ip);
+
+        while (ss >> number) {
+            val.push_back(number);
+        }
+
+        w = arr[0];
+        n = val.size();
+        getline(cin, ip);
+        ss.clear();
+        ss.str(ip);
+
+        while (ss >> number) {
+            wt.push_back(number);
+        }
+        Solution ob;
+        cout << ob.knapSack(w, wt, val) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends

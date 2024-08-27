5ac57114fed98535a259878587e783f180a8bd1e
nikhil980 <154825911+nikhil980@users.noreply.github.com>
2024-08-27 15:58:09 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Merging two unsorted arrays in sorted order/merging-two-unsorted-arrays-in-sorted-order.cpp
+++ Difficulty: Easy/Merging two unsorted arrays in sorted order/merging-two-unsorted-arrays-in-sorted-order.cpp
@@ -2,72 +2,83 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
 // } Driver Code Ends
 // Your code goes hereclass Solution{
-	
-class Solution{
-	public:
-	void sortedMerge(int a[], int b[], int res[],int n, int m)
-	{
-	   int a1=0;
-	   int b1=0;
-	   sort(a,a+n);
-	   sort(b,b+m);
-	  
-	   for(int i=0;i<n+m;i++)
-	   {
-	       if(a[a1]<=b[b1]&&a1<n)
-	       {
-	           res[i]=a[a1];
-	           a1++;
-	       }
-	       else
-	       {
-	            res[i]=b[b1];
-	            b1++;
-	       }
-	       
-	   }
-	      
-	   
-	}
+
+class Solution {
+  public:
+    void sortedMerge(vector<int>& arr1, vector<int>& arr2, vector<int>& res) 
+    {
+       sort(arr1.begin(),arr1.end());
+       sort(arr2.begin(),arr2.end());
+       int i=0;
+       int j=0;
+       int k=0;
+       while(j<arr2.size()&&i<arr1.size())
+       {
+           if(arr1[i]<=arr2[j])
+           {
+               res[k]=arr1[i];
+               i++;
+               k++;
+               continue;
+           }
+          if(arr1[i]>=arr2[j])
+           {
+                res[k]=arr2[j];
+               k++;
+               j++;
+               continue;
+           }
+       }
+       while(i<arr1.size())
+       {
+        res[k]=arr1[i];
+        k++;
+               i++;
+       }
+        while(j<arr2.size())
+       {
+         res[k]=arr2[j];
+         k++;
+               j++;
+       }
+    }
 };
-	  
 
 
 //{ Driver Code Starts.
-
-int main() 
-{
-   	
-   	int t;
-    cin >> t;
-    while (t--)
-    {
-    	int n, m;
-	    cin >> n >> m;
-	    int a[n], b[m];
-	    for(int i = 0;i<n;i++){
-	        cin >> a[i];
-	    }
-
-	    for(int i = 0;i<m;i++){
-	        cin >> b[i];
-	    }
-	    
-	    int res[n+m];
-
-      
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        string s;
+        getline(cin, s);
+        stringstream ss(s);
+        vector<int> arr1, arr2;
+        int num;
+        while (ss >> num) {
+            arr1.push_back(num);
+        }
+        getline(cin, s);
+        ss.clear();
+        ss.str(s);
+        while (ss >> num) {
+            arr2.push_back(num);
+        }
+        int n = arr1.size();
+        int m = arr2.size();
+        vector<int> res(n + m);
         Solution ob;
-        ob.sortedMerge(a,b,res,n,m);
+        ob.sortedMerge(arr1, arr2, res);
 
-        for(int i = 0;i<n+m;i++){
-	        cout << res[i] << " ";
-	    }
-	   
-        
-	    cout << "\n";
-	     
+        for (int i = 0; i < n + m; i++) {
+            cout << res[i] << " ";
+        }
+
+        cout << "\n";
     }
     return 0;
 }

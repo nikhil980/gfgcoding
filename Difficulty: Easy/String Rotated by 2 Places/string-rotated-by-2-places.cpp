524e17bf03424cf2dc00e2a4d76ab18d81cedaf7
nikhil980 <154825911+nikhil980@users.noreply.github.com>
2024-09-05 14:43:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/String Rotated by 2 Places/string-rotated-by-2-places.cpp
@@ -0,0 +1,107 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to check if a string can be obtained by rotating
+    //another string by exactly 2 places.
+    bool isRotated(string str1, string str2)
+    {
+        int x=1;
+        int z=1;
+         int n1 = str1.size(), n2 = str2.size();
+        if(n1!=n2)
+        {
+            return false;
+        }
+        if(n1 <= 2) return (str1 == str2);
+        
+        int i=2;
+        int j=0;
+        
+         while(i<str1.length())
+         {
+             if(str1[i]!=str2[j])
+             {
+                 x=0;
+                 break;
+             }
+             else
+             {
+                 i++;
+                 j++;
+             }
+         }
+        
+         if(x!=0){
+              i=0;
+         
+         while(j<str2.length())
+         {
+              if(str1[i]!=str2[j])
+             {
+                 x=0;
+                 break;
+             }
+             i++;
+             j++;
+         }
+    }
+         int l=str1.size()-2;
+         int m=0;
+        
+         while(l<str1.length())
+         {
+             if(str1[l]!=str2[m])
+             {
+                 z=0;
+                 break;
+             }
+             else
+             {
+                 l++;
+                 m++;
+             }
+         }
+       
+         if(z!=0){
+               l=0;
+         while(m<str2.length())
+         {
+              if(str1[l]!=str2[m])
+             {
+                 z=0;
+                 break;
+             }
+             l++;
+             m++;
+         }
+         }
+         return (x||z);
+    }
+
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+	
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		string s;
+		string b;
+		cin>>s>>b;
+		Solution obj;
+		cout<<obj.isRotated(s,b)<<endl;
+	}
+	return 0;
+}
+
+// } Driver Code Ends

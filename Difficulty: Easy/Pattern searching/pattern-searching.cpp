0662c77ac87a685ba83bd2611dc909b3adde2dfa
nikhil980 <154825911+nikhil980@users.noreply.github.com>
2024-09-07 11:16:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Pattern searching/pattern-searching.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+/*
+str: string in which pattern we have to find pattern.
+pat: pattern needs to searched.
+*/
+
+bool searchPattern(string str, string pat)
+{
+    if(str.find(pat)!=string::npos)
+	    {
+	        return 1;
+	    }
+	    else
+	    {
+	        return 0;
+	    }
+}
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        string str, pat;
+        cin >> str;
+        cin >> pat;
+
+        if (searchPattern(str, pat) == true)
+            cout << "Present" << endl;
+        else
+            cout << "Not present" << endl;
+    }
+}
+
+// } Driver Code Ends

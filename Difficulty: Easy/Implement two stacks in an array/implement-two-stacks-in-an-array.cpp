0cb5fa77a8d3c7126dd0e8d5bba62d75b0bd597f
nikhil980 <154825911+nikhil980@users.noreply.github.com>
2024-09-09 05:17:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Implement two stacks in an array/implement-two-stacks-in-an-array.cpp
@@ -0,0 +1,93 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+class twoStacks {
+  public:
+  int arr[100];
+  int top1=-1;
+  int top2=100;
+    twoStacks(){}
+
+    // Function to push an integer into the stack1.
+    void push1(int x) 
+    {
+        if(top1<top2)
+          top1++;
+          arr[top1]=x;
+    }
+
+    // Function to push an integer into the stack2.
+    void push2(int x)
+    {
+        if(top2>top1){
+        top2--;
+        arr[top2]=x;}
+    }
+
+    // Function to remove an element from top of the stack1.
+    int pop1()
+    {
+        if(top1!=-1){
+        int x=arr[top1];
+        top1--;
+        return x;}
+        else
+        return -1;
+        
+    }
+
+    // Function to remove an element from top of the stack2.
+    int pop2()
+    {
+        if(top2<100){
+        int x=arr[top2];
+        top2++;
+        return x;}
+        else
+        return -1;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int T;
+    cin >> T;
+    while (T--) {
+        twoStacks *sq = new twoStacks();
+
+        int Q;
+        cin >> Q;
+        while (Q--) {
+            int stack_no;
+            cin >> stack_no;
+            int QueryType = 0;
+            cin >> QueryType;
+
+            if (QueryType == 1) {
+                int a;
+                cin >> a;
+                if (stack_no == 1)
+                    sq->push1(a);
+                else if (stack_no == 2)
+                    sq->push2(a);
+            } else if (QueryType == 2) {
+                if (stack_no == 1)
+                    cout << sq->pop1() << " ";
+                else if (stack_no == 2)
+                    cout << sq->pop2() << " ";
+            }
+        }
+        cout << endl;
+    }
+}
+
+// } Driver Code Ends

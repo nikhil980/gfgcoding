617ef02d661aac606f51fd76cea616530899dc53
nikhil980 <154825911+nikhil980@users.noreply.github.com>
2024-10-14 08:49:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Segregate even and odd nodes in a Linked List/segregate-even-and-odd-nodes-in-a-linked-list.cpp
@@ -0,0 +1,119 @@
+//{ Driver Code Starts
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node *next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+void printList(Node *node) {
+    while (node != NULL) {
+        cout << node->data << " ";
+        node = node->next;
+    }
+    cout << "\n";
+}
+
+
+// } Driver Code Ends
+// User function template for C++
+
+/*
+struct Node
+{
+    int data;
+    struct Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+class Solution {
+  public:
+    Node* divide(Node* head) {
+            vector<int>even;
+      vector<int>odd;
+      
+      struct Node *temp=head;
+      
+      while(temp)
+      {
+          if(temp->data%2==0)
+          {
+              even.push_back(temp->data);
+          }
+          else
+          {
+              odd.push_back(temp->data); 
+          }
+          temp=temp->next;
+      }
+      temp=head;
+      int i=0,j=0;
+      while(temp)
+      {
+          if(i<even.size())
+          {
+          temp->data=even[i];
+          i++;
+          }
+          else{
+              temp->data=odd[j];
+              j++;
+          }
+          temp=temp->next;
+      }
+      return head;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+    cin >> t;
+    cin.ignore(); // To ignore the newline character after the integer input
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        if (arr.empty())
+            continue;
+
+        int data = arr[0];
+        int N = arr.size();
+        struct Node *head = new Node(data);
+        struct Node *tail = head;
+        for (int i = 1; i < N; ++i) {
+            data = arr[i];
+            tail->next = new Node(data);
+            tail = tail->next;
+        }
+
+        Solution ob;
+        Node *ans = ob.divide(head);
+        printList(ans);
+    }
+    return 0;
+}
+
+// } Driver Code Ends

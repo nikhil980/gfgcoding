2522cd16a38cf352aaf0d902c3c97d3c7e810989
nikhil980 <154825911+nikhil980@users.noreply.github.com>
2024-10-19 04:48:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Nearest multiple of 10/nearest-multiple-of-10.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+#include <iostream>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    string roundToNearest(string str) {
+        // Complete the function
+           int n=str.length();
+        if(str[n-1]<='5')
+        {
+            str[n-1]='0';
+            return str;
+        }
+        str[n-1]='0';
+        int i=n-2;
+        while(i>=0 && str[i]=='9')
+        {
+            str[i]='0';
+            i--;
+        }
+        if(i<0)
+        str='1'+str;
+        else
+        str[i]++;
+        return str;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+    cin >> t;
+    while (t--) {
+        string str;
+        cin >> str;
+        Solution ob;
+        cout << ob.roundToNearest(str) << endl;
+    }
+
+    return 0;
+}
+// } Driver Code Ends

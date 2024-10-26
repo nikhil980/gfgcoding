b52ada115a4006083291df7f665a9a59bcb224ba
nikhil980 <154825911+nikhil980@users.noreply.github.com>
2024-10-27 04:43:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Triplet Family/triplet-family.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    bool findTriplet(vector<int>& arr) {
+        // Your code
+        unordered_set<int>h(arr.begin(),arr.end());
+        
+        for(int i=0;i<arr.size();i++)
+        {
+            for(int j=0;j<arr.size();j++)
+            {
+                if(i==j)
+                {
+                    continue;
+                }
+                if(h.find(arr[i]+arr[j])!=h.end())
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        bool res = obj.findTriplet(arr);
+        if (res)
+            cout << "true" << endl;
+        else
+            cout << "false" << endl;
+        // cout << res << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends

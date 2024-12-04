a878307d975f76c3e4148a763b5b05cad4f3fbe2
nikhil980 <154825911+nikhil980@users.noreply.github.com>
2024-12-04 06:02:47 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Strings Rotations of Each Other/strings-rotations-of-each-other.cpp
+++ Difficulty: Easy/Strings Rotations of Each Other/strings-rotations-of-each-other.cpp
@@ -5,14 +5,12 @@ using namespace std;
 
 // } Driver Code Ends
 
-class Solution
-{
-    public:
-    //Function to check if two strings are rotations of each other or not.
-  bool areRotations(string s1,string s2)
-    {
+class Solution {
+  public:
+    // Function to check if two strings are rotations of each other or not.
+    bool areRotations(string &s1, string &s2) {
         // Your code here
-        if(s2.length() != s1.length()) return false;
+         if(s2.length() != s1.length()) return false;
         
         string concat = s1+s1;
         
@@ -20,24 +18,28 @@ class Solution
         return true;
         
         return false;
-    
     }
 };
 
 //{ Driver Code Starts.
 
-int main()
-{
+int main() {
     int t;
-    cin>>t;
-    while(t--)
-    {
+    cin >> t;
+    while (t--) {
         string s1;
         string s2;
-        cin>>s1>>s2;
-        Solution obj;
-        cout<<obj.areRotations(s1,s2)<<endl;
+        cin >> s1 >> s2;
+        Solution sol;
+        int result = sol.areRotations(s1, s2);
+        if (result) {
+            cout << "true" << endl;
 
+        } else {
+            cout << "false" << endl;
+        }
+        cout << "~"
+             << "\n";
     }
     return 0;
 }

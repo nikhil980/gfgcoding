aa57a45a16409c2ff28a578cd48849aafd8f35bd
nikhil980 <154825911+nikhil980@users.noreply.github.com>
2024-12-08 05:42:27 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Overlapping Intervals/overlapping-intervals.cpp
+++ Difficulty: Medium/Overlapping Intervals/overlapping-intervals.cpp
@@ -1,59 +1,62 @@
 //{ Driver Code Starts
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+
 
 // } Driver Code Ends
 class Solution {
-public:
-    vector<vector<int>> overlappedInterval(vector<vector<int>>& intervals)
-    {
-     sort(intervals.begin(),intervals.end());
-     
-     vector<vector<int>>ans;
-     ans.push_back(intervals[0]);
-     int j=0;
-     for(int i=1;i<intervals.size();i++)
-     {
-         if(ans[j][1]>=intervals[i][0])
-         {
-             ans[j][0]=min(ans[j][0],intervals[i][0]);
-             ans[j][1]=max(ans[j][1],intervals[i][1]);
-         }
-         else
-         {
-              ans.push_back(intervals[i]);
-              j++;
-         }
-     }
-     
-     return ans;
-         
+  public:
+      vector<vector<int>> mergeOverlap(vector<vector<int>>& arr) {
+    
+    sort(arr.begin(), arr.end());
+
+    vector<vector<int>> merged;
+    merged.push_back(arr[0]); 
+
+    for (int i = 1; i < arr.size(); i++) {
+       
+        int n=merged.size();
+        vector<int>& last = merged[n-1]; 
+        vector<int> curr = arr[i];
+
+        if (curr[0] <= last[1]) { // Overlap condition
+            
+            last[1] = max(last[1], curr[1]);
+        } 
+        
+        else { // No overlap, add current interval
+            
+            merged.push_back(curr);
+        }
     }
+
+    return merged;
+}
 };
 
 //{ Driver Code Starts.
-int main(){
-	int tc;
-	cin >> tc;
-	while(tc--){
-		int n;
-		cin >> n;
-		vector<vector<int>>Intervals(n);
-		for(int i = 0; i < n; i++){
-			int x, y;
-			cin >> x >> y;
-			Intervals[i].push_back(x);
-			Intervals[i].push_back(y);
-		}
-		Solution obj;
-		vector<vector<int>> ans = obj.overlappedInterval(Intervals);
-		for(auto i: ans){
-			for(auto j: i){
-				cout << j << " ";
-			}
-		}
-		cout << "\n";
-	}
-	return 0;
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int n;
+        cin >> n;
+        vector<vector<int>> arr(n);
+        for (int i = 0; i < n; i++) {
+            int x, y;
+            cin >> x >> y;
+            arr[i].push_back(x);
+            arr[i].push_back(y);
+        }
+        Solution obj;
+        vector<vector<int>> ans = obj.mergeOverlap(arr);
+        for (auto i : ans) {
+            for (auto j : i) {
+                cout << j << " ";
+            }
+        }
+        cout << "\n";
+    }
+    return 0;
 }
 // } Driver Code Ends

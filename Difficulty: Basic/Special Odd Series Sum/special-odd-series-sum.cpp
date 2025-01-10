f6e6e74c9b79f4a8430c4b70523b8d03ff6e5f9b
nikhil980 <154825911+nikhil980@users.noreply.github.com>
2025-01-10 20:43:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Special Odd Series Sum/special-odd-series-sum.cpp
@@ -0,0 +1,30 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    long long int sumOfTheSeries(long long int n){
+        // code here
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long int n;
+        cin >> n;
+        Solution ob;
+        cout << ob.sumOfTheSeries(n) << endl;
+    
+cout << "~" << "\n";
+}
+    return 0;
+}
+
+// } Driver Code Ends

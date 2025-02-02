7120ecc941b817831b9e42c0ad04e0ebbb2b3947
nikhil980 <154825911+nikhil980@users.noreply.github.com>
2025-02-02 09:50:04 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Level order traversal/level-order-traversal.cpp
+++ Difficulty: Easy/Level order traversal/level-order-traversal.cpp
@@ -4,72 +4,68 @@ using namespace std;
 
 /* A binary tree node has data, pointer to left child
    and a pointer to right child */
-struct Node
-{
+class Node {
+  public:
     int data;
-    struct Node* left;
-    struct Node* right;
+    Node* left;
+    Node* right;
+
+    // Constructor
+    Node(int val) {
+        data = val;
+        left = nullptr;
+        right = nullptr;
+    }
 };
+
 // Utility function to create a new Tree Node
-Node* newNode(int val)
-{
-    Node* temp = new Node;
-    temp->data = val;
-    temp->left = NULL;
-    temp->right = NULL;
-    
-    return temp;
+Node* newNode(int val) {
+    return new Node(val);
 }
+
 
 // } Driver Code Ends
 /* A binary tree Node
-
-struct Node
-{
+class Node {
+  public:
     int data;
-    struct Node* left;
-    struct Node* right;
-    
-    Node(int x){
-        data = x;
-        left = right = NULL;
+    Node* left;
+    Node* right;
+
+    // Constructor
+    Node(int val) {
+        data = val;
+        left = nullptr;
+        right = nullptr;
     }
 };
- */
+*/
 
-
-class Solution
-{
-    public:
-    //Function to return the level order traversal of a tree.
-    vector<int> levelOrder(Node* root)
-    {
-     queue<Node*>ans;
-     ans.push(root);
-     vector<int>a;
-     if(root==NULL)
-     {
-         return a;
-     }
-     while(!ans.empty())
-     {
-           Node *temp=ans.front();
-           ans.pop();
-           a.push_back(temp->data);
-           
-           if(temp->left!=NULL)
-           {
-               ans.push(temp->left);
-           }
-            if(temp->right!=NULL)
-           {
-               ans.push(temp->right);
-           }
-           
-       }
-       return a;
+class Solution {
+  public:
+    vector<vector<int>> levelOrder(Node *root) {
+        // Your code here
+        vector<vector<int>> ans;
+        queue<Node*>q;
+        q.push(root);
+        
+        while(!q.empty())
+        {
+            int k=q.size();
+            vector<int>j;
+            for(int i=0;i<k;i++)
+            {
+                Node * temp=q.front();
+                q.pop();
+                j.push_back(temp->data);
+                if(temp->left!=NULL) q.push(temp->left);
+                if(temp->right!=NULL) q.push(temp->right);
+                
+            }
+            ans.push_back(j);
+        }
+        return ans;
     }
-    
 };
 
 //{ Driver Code Starts.
@@ -77,99 +73,97 @@ class Solution
 /* Helper function to test mirror(). Given a binary
    search tree, print out its data elements in
    increasing sorted order.*/
-void inOrder(struct Node* node)
-{
-  if (node == NULL)
-    return;
+void inOrder(Node* node) {
+    if (node == nullptr)
+        return;
 
-  inOrder(node->left);
-  printf("%d ", node->data);
+    inOrder(node->left);
+    printf("%d ", node->data);
 
-  inOrder(node->right);
+    inOrder(node->right);
 }
 
 // Function to Build Tree
-Node* buildTree(string str)
-{   
+Node* buildTree(string str) {
     // Corner Case
-    if(str.length() == 0 || str[0] == 'N')
-            return NULL;
-    
-    // Creating vector of strings from input 
+    if (str.length() == 0 || str[0] == 'N')
+        return nullptr;
+
+    // Creating vector of strings from input
     // string after spliting by space
     vector<string> ip;
-    
+
     istringstream iss(str);
-    for(string str; iss >> str; )
+    for (string str; iss >> str;)
         ip.push_back(str);
-        
+
     // Create the root of the tree
     Node* root = newNode(stoi(ip[0]));
-        
+
     // Push the root to the queue
     queue<Node*> queue;
     queue.push(root);
-        
+
     // Starting from the second element
     int i = 1;
-    while(!queue.empty() && i < ip.size()) {
-            
+    while (!queue.empty() && i < ip.size()) {
+
         // Get and remove the front of the queue
         Node* currNode = queue.front();
         queue.pop();
-            
+
         // Get the current node's value from the string
         string currVal = ip[i];
-            
+
         // If the left child is not null
-        if(currVal != "N") {
-                
+        if (currVal != "N") {
+
             // Create the left child for the current node
             currNode->left = newNode(stoi(currVal));
-                
+
             // Push it to the queue
             queue.push(currNode->left);
         }
-            
+
         // For the right child
         i++;
-        if(i >= ip.size())
+        if (i >= ip.size())
             break;
         currVal = ip[i];
-            
+
         // If the right child is not null
-        if(currVal != "N") {
-                
+        if (currVal != "N") {
+
             // Create the right child for the current node
             currNode->right = newNode(stoi(currVal));
-                
+
             // Push it to the queue
             queue.push(currNode->right);
         }
         i++;
     }
-    
+
     return root;
 }
 
 /* Driver program to test size function*/
-int main()
-{
-  int t;
-  scanf("%d ",&t);
-  while (t--)
-  {
+int main() {
+    int t;
+    scanf("%d ", &t);
+    while (t--) {
         string s;
-		getline(cin,s);
-		Node* root = buildTree(s);
-		Solution ob;
-        vector <int> res = ob.levelOrder(root);
-        for (int i : res) cout << i << " ";
+        getline(cin, s);
+        Node* root = buildTree(s);
+        Solution ob;
+        vector<vector<int>> res = ob.levelOrder(root);
+        for (auto level : res) {
+            for (int ele : level)
+                cout << ele << " ";
+        }
         cout << endl;
-  }
-  return 0;
+        cout << "~" << endl;
+    }
+    return 0;
 }
-
-
 
 // } Driver Code Ends

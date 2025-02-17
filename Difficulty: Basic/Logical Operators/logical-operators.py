fb6e6396858cf962794223fc27c54de9aa83566f
nikhil980 <154825911+nikhil980@users.noreply.github.com>
2025-02-17 19:35:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Logical Operators/logical-operators.py
@@ -0,0 +1,25 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+#User function Template for python3
+#The two lines below take input. Don't change them!
+a=int(input())
+b=int(input())
+
+########### Write your code below ###############
+#Do a and b below
+p = a and b
+#Do a or b below
+q = a or b
+#Do not a below
+r = not a
+########### Write your code above ###############
+
+#The code below prints the output. Don't change it!
+print(p,q,r)
+
+#{ 
+ # Driver Code Starts.
+# } Driver Code Ends

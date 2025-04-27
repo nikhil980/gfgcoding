8779c397d0e1625edd32caddf0f973825b2ca01d
nikhil980 <154825911+nikhil980@users.noreply.github.com>
2025-04-27 22:16:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Multiply two strings/multiply-two-strings.py
@@ -0,0 +1,21 @@
+#User function Template for python3
+
+class Solution:
+    def multiplyStrings(self, s1, s2):
+        return str(int(s1)*int(s2))
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == "__main__":
+    t = int(input())
+    for i in range(t):
+        a = input()
+        b = input()
+        print(Solution().multiplyStrings(a.strip(), b.strip()))
+
+        print("~")
+
+# } Driver Code Ends

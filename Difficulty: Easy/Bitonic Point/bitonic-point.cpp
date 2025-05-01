8095c3219925445f516131c9cd9eaed78233ca1a
nikhil980 <154825911+nikhil980@users.noreply.github.com>
2025-05-02 05:14:34 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Bitonic Point/bitonic-point.cpp
+++ Difficulty: Easy/Bitonic Point/bitonic-point.cpp
@@ -1,36 +1,54 @@
 //{ Driver Code Starts
 #include <bits/stdc++.h>
-
 using namespace std;
 
 
 // } Driver Code Ends
-//User function template for C++
-class Solution{
-public:
-	
-	int findMaximum(int arr[], int n)
-	{
-	   sort(arr,arr+n);
-	    return arr[n-1];
-	}
+
+// User function template for C++
+class Solution {
+  public:
+    int findMaximum(vector<int> &arr) {
+        // code here
+        
+        int currmax=INT_MIN;
+        for(auto i:arr)
+        {
+            currmax=max(currmax,i);
+        }
+        return currmax;
+        
+    }
 };
+
 
 //{ Driver Code Starts.
 
 int main() {
     int t;
     cin >> t;
+    cin.ignore();
+
     while (t--) {
-        int n, i;
-        cin >> n;
-        int arr[n];
-        for (i = 0; i < n; i++) {
-            cin >> arr[i];
+        vector<int> arr;
+        int value;
+
+        // Read the entire line of integers
+        string line;
+        // cin.ignore(); // Ignore the newline after the test case input
+        getline(cin, line);
+        stringstream ss(line);
+
+        // Parse integers and add them to the vector
+        while (ss >> value) {
+            arr.push_back(value);
         }
+
         Solution ob;
-        auto ans = ob.findMaximum(arr, n);
+        auto ans = ob.findMaximum(arr);
         cout << ans << "\n";
+        cout << "~"
+             << "\n";
     }
     return 0;
 }
